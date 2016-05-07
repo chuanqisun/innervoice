332e154d0642e138204e60f153388dfc15b5e9dc
Chuanqi <henrysun918@gmail.com>
2016-05-07 13:43:38 -0700

    update site structure and marketing page

--- main.js
+++ main.js
@@ -1,94 +1,393 @@
-init = function() {
-    // scope var
-    var playlist;
-    var tracks;
-    var displayedPlaylist;
-    var player;
-    var currentTrack = 0;
-
-    // LOCAL DEBUG ONLY
-    $.getJSON = function(str, callback) {
-        callback();
+// ==================== App ====================
+IV = (function() {
+    function init(config) {
+        IV.Constants.init(config);
+        IV.Service.init(onServiceReady);
     }
 
-    // Get playlist data
-    $.getJSON("playlist.json", function(playlists) {
-        playlists = 
-        [{
-            "volume": "1",
-            "title": "Northern Whispers",
-            "dates": "2016.May.4",
-            "intro": "Softly they whisper, so as not to be found by the ancient sorrows.",
-            "tracks": [
-                {"artist":"Radical Face", "name":"Gray Skies", "path":"./audio/001/01-Gray_Skies.mp3", "length":"00:43"},
-                {"artist":"Carved In Stone", "name":"Die Gärten der Feen", "path":"./audio/001/02-Die_Gaerten_Der_Feen.mp3", "length":"03:03"},
-                {"artist":"Adaro", "name":"Es Ist Ein Schnee Gefallen", "path":"./audio/001/03-Es_ist_ein_Schnee_gefallen.mp3", "length":"03:59"},
-                {"artist":"Agnes Obel", "name":"Riverside", "path":"./audio/001/04-Riverside.mp3", "length":"03:51"},
-                {"artist":"Mono", "name":"Burial At Sea", "path":"./audio/001/05-Burial_at_the_sea.mp3", "length":"10:40"}
-            ]
-        }];
+    function onServiceReady() {
+        IV.Model.init();
+        IV.View.init();
+        IV.Controller.init();
+        IV.Router.init();
+    }
 
-        playlist = playlists[0];
-        tracks = playlist.tracks;
+    return {
+        init: init
+    }
+}.call({}));
 
-        // Attach audio source
-        currentTrack = 0;
-        player = $("<audio></audio>").attr({
-            'src':tracks[currentTrack].path,
-            'volume':1,
-        }).appendTo("footer")[0];
+// ==================== Constants ====================
+IV.Constants = (function() {
+    var currentVolumnNumber;
+    var audioPath;
+    var volumePathWidth;
+    var debug;
+    var playlistFilename;
 
-        // Render title, volume, intro, tracks
-        $('.title').text(playlist.title);
-        $('.volume').text('vol. ' + playlist.volume);
-        $('.intro').text(playlist.intro);
-        displayedPlaylist = $('ol.playlist');
-        $.each(tracks, function(i)
-        {
-            var li = $('<li/>').text(tracks[i].name).attr('artist', tracks[i].artist).appendTo(displayedPlaylist);
-            li.click(function(e){
-                e.preventDefault();
-                playTrack(i, tracks, displayedPlaylist, player);
-            });
-        });
+    function init(config) {
+        currentVolumnNumber = config.currentVolumnNumber;
+        audioPath = config.audioPath;
+        volumePathWidth = config.volumePathWidth;
+        debug = config.debug;
+        playlistFilename = config.playlistFilename;
+    }
 
-        // Autoplay next track
-        player.addEventListener('ended',function(e){
-            currentTrack = (currentTrack + 1) % tracks.length;   
-            playTrack(currentTrack, tracks, displayedPlaylist, player);
-        });
+    return {
+        init: init,
+        get currentVolumnNumber() { return currentVolumnNumber; },
+        get audioPath() { return audioPath; },
+        get volumePathWidth() { return volumePathWidth; },
+        get debug() { return debug; },
+        get playlistFilename() { return playlistFilename; }
+    }
+}.call({}));
 
-        // Start playing when first visit tracks page
-        var onHashChange = function() {
-            if (location.hash === '#tracks' && player.paused)
-                playTrack(currentTrack, tracks, displayedPlaylist, player);
-            $(window).off('hashchange', undefined, onHashChange);
-        }
-        $(window).on('hashchange', undefined, onHashChange);
+// ==================== Service ====================
+IV.Service = (function() {
+    var playlistsResponse;
+    var initCallback;
+    var detectTouchDeviceResult;
+    var detectMobileDeviceResult;
 
-        // Attach play/pause to icon
-        $('.play-icon').click(function() {
-            if (!player.paused) {
-                player.pause();
-                $('.play-icon').addClass('paused');
-            } else {
-                player.play();
-                $('.play-icon').removeClass('paused');
+    function init(callback) {
+        initCallback = callback;
+
+        // Overwrite getJSON for local DEBUG
+        if (IV.Constants.debug) {
+            $.getJSON = function(str, callback) {
+                mockResult = 
+                [{
+                    "volume": "1",
+                    "title": "The Ancient Sorrow",
+                    "dates": "1462651292",
+                    "intro": "Softly they whispered into darkness, but The Ancient Sorrow echoed with a beam of light.",
+                    "tracks": [
+                        {"artist":"Radical Face", "name":"Gray Skies", "filename":"Gray_Skies.mp3", "length":"00:43"},
+                        {"artist":"Carved In Stone", "name":"Die Gärten der Feen", "filename":"Die_Gaerten_Der_Feen.mp3", "length":"03:03"},
+                        {"artist":"Adaro", "name":"Es Ist Ein Schnee Gefallen", "filename":"Es_ist_ein_Schnee_gefallen.mp3", "length":"03:59"},
+                        {"artist":"Agnes Obel", "name":"Riverside", "filename":"Riverside.mp3", "length":"03:51"},
+                        {"artist":"Mono", "name":"Burial At Sea", "filename":"Burial_at_the_sea.mp3", "length":"10:40"}
+                    ]
+                }];
+                window.setTimeout(function(){
+                    callback(mockResult);
+                }, 1000);
             }
+        }
+
+        $.getJSON(IV.Constants.playlistFilename, onDataReady);
+        detectTouchDevice();
+    }
+
+    function onDataReady(data) {
+        IV.Service.playlists = data;
+        initCallback();
+    }
+
+    function detectTouchDevice() {
+        detectTouchDeviceResult =  'ontouchstart' in document.documentElement;
+    }
+
+    function detectMobileDevice() {
+         detectMobileDeviceResult = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+    }
+
+    return {
+        init: init,
+        get playlists() { return playlistsResponse; },
+        set playlists(data) { playlistsResponse = data; },
+        get isTouchDevice() { return detectTouchDeviceResult; },
+        get isMobileDevice() { return detectMobileDeviceResult; }
+    };
+}.call({}));
+
+// ==================== Router ====================
+IV.Router = (function() {
+    var oneTimeAutoPlayFunc; 
+    var oneTimeShowAudioControlFunc;
+    var oneTimeRevealHeadphoneNavControlFunc;
+    var oneTimeRevealIntroNavControlFunc;
+
+    function init() {
+        oneTimeAutoPlayFunc = _.once(IV.Controller.playTrackByTrackNumber);
+        oneTimeShowAudioControlFunc = _.once(IV.View.showAudioControl);
+        oneTimeRevealHeadphoneNavControlFunc = _.once(IV.View.revealHeadphoneNavControl);
+        oneTimeRevealIntroNavControlFunc = _.once(IV.View.revealIntroNavControl);
+
+        $(window).on('hashchange', onHashChange);
+        onHashChange();
+    }
+
+    function onHashChange() {
+        switch (location.hash) {
+            case '#tracks':
+                onEnterTracksView();
+                break;
+            case '#intro':
+                onEnterIntroView();
+                break;
+            case '#headphone':
+            case '':
+                onEnterHeadphoneView();
+                break;
+        }
+    }
+
+    function onEnterTracksView() {
+        oneTimeShowAudioControlFunc();
+        oneTimeAutoPlayFunc(IV.Model.currentTrackNumber, true);
+    }
+
+    function onEnterIntroView() {
+        oneTimeRevealIntroNavControlFunc();
+    }
+
+    function onEnterHeadphoneView() {
+        oneTimeRevealHeadphoneNavControlFunc();
+    }
+
+    return {
+        init: init
+    };
+}.call({}));
+
+// ==================== Model ====================
+IV.Model = (function() {
+    var playlist;
+    var tracks;
+    var currentTrackNumber;
+    var nextTrackNumber;
+
+    function init() {
+        playlist = _.find(IV.Service.playlists, {'volume': IV.Constants.currentVolumnNumber.toString()});
+        tracks = playlist.tracks;
+        currentTrackNumber = 0;
+    }
+
+    function setCurrentTrackByTrackNumber(newTrackNumber) {
+        currentTrackNumber = newTrackNumber;
+    }
+
+    function getPathForTrack(track) {
+        var volumnePath = convertNumberToStringWithWidth(IV.Constants.currentVolumnNumber, IV.Constants.volumePathWidth) + '/';
+        return IV.Constants.audioPath + volumnePath + track.filename;
+    }
+
+    function convertNumberToStringWithWidth(number, width) {
+        var result = Math.pow(10, width);
+        if (number > result || width < 1) {
+            return number.toString(); // overflow, don't convert
+        } else {
+            number += result;
+            return number.toString().slice(1);
+        }
+    }
+
+    return {
+        init: init,
+        setCurrentTrackByTrackNumber: setCurrentTrackByTrackNumber,
+        getPathForTrack: getPathForTrack,
+        get playlist() { return playlist; },
+        get tracks() { return tracks; },
+        get currentTrackNumber() { return currentTrackNumber; },
+        get currentTrack() { return tracks[currentTrackNumber]; },
+        get nextTrackNumber() { return  (currentTrackNumber + 1) % tracks.length; }
+    }
+}.call({}));
+
+// ==================== View ====================
+IV.View = (function() {
+    var $audio;
+    var $audioControl;
+    var $footer;
+    var $title;
+    var $volume;
+    var $intro;
+    var $tracksContainer;
+    var $displayedPlaylist;
+    var $currentTrack;
+    var $navControl;
+    var $headphoneNavControl;
+    var $introNavControl;
+
+    function init() {
+        fetchTemplate();
+        hydrateTemplate();
+        bindInteractions();
+    }
+
+    function fetchTemplate() {
+        $audio = $('<audio></audio>');
+        $audioControl = $('.audio-control');
+        $footer = $('footer');
+        $title = $('.title');
+        $volume = $('.volume');
+        $intro = $('.intro');
+        $tracksContainer = $('.tracks-container');
+        $displayedPlaylist = $('ol.tracks');
+        $navControl = $('.nav-container a');
+        $headphoneNavControl = $('#headphone .nav-container a');
+        $introNavControl = $('#intro .nav-container a');
+    }
+
+    function hydrateTemplate() {
+        $footer.append(hydrateAudioPlayer());
+        $title.text(IV.Model.playlist.title);
+        $volume.text('vol. ' + IV.Model.playlist.volume);
+        $intro.text(IV.Model.playlist.intro);
+        $tracksContainer.prepend(hydrateDisplayedPlaylist());
+    }
+
+    function bindInteractions() {
+        var $displayedPlaylistItems = $displayedPlaylist.find('li');
+        enableHover($displayedPlaylistItems);
+        enableHover($navControl);
+        handleTracksOnClick($displayedPlaylistItems);
+        handleAudioPlayerOnClick();
+    }
+
+    function hydrateAudioPlayer() {
+        return $audio.attr({
+            'src': IV.Model.getPathForTrack(IV.Model.currentTrack),
+            'volume': 1,
+            'preload': 'none'
+        });
+    }
+
+    function hydrateDisplayedPlaylist() {
+        _.each(IV.Model.tracks, function(track)
+        {
+            $('<li/>').text(track.artist + ' · ' + track.name).appendTo($displayedPlaylist);
         });
 
-        // Start playing
-        playTrack(currentTrack, tracks, displayedPlaylist, player);
-    });
-}
+        return $displayedPlaylist;
+    }
 
-function playTrack(trackNumber, tracks, displayedPlaylist, player){
-    displayedTrack = displayedPlaylist.find('li').eq(trackNumber);
-    displayedTrack.addClass('playing').siblings().removeClass('playing');
-    player.src = tracks[trackNumber].path;
-    player.load();
-    player.play();
-}
+    function enableHover(selector) {
+        if (!IV.Service.isTouchDevice) {
+            selector.hover(
+                function(){
+                    $(this).addClass('hover');
+                }, 
+                function(){
+                    $(this).removeClass('hover');
+                }
+            );
+        }
+    }
+
+    function handleTracksOnClick(displayedPlaylistItems) {
+        displayedPlaylistItems.click(function(e){
+            e.preventDefault();
+            IV.Controller.playTrackByTrackNumber($(e.target).index());
+        })
+    }
+
+    function handleAudioPlayerOnClick() {
+        $audioControl.click(function(e) {
+            e.preventDefault();
+            IV.Controller.togglePlay();
+        });
+    }
+
+    function showAudioControl() {
+        $audioControl.removeClass('hide');
+    }
+
+    function revealHeadphoneNavControl() {
+        $headphoneNavControl.removeClass('invisible');
+    }
+
+    function revealIntroNavControl() {
+        $introNavControl.removeClass('invisible');
+    }
+
+    function highlighCurrentTrack() {
+        var $currentTrack = $displayedPlaylist.find('li').eq(IV.Model.currentTrackNumber);
+        $currentTrack.addClass('playing').siblings().removeClass('playing');
+    }
+
+    function startAudioControlAnimation() {
+        $audioControl.removeClass('paused');
+    }
+
+    function pauseAudioControlAnimation() {
+        $audioControl.addClass('paused');
+    }
+
+    return {
+        init: init,
+        showAudioControl: showAudioControl,
+        revealHeadphoneNavControl: revealHeadphoneNavControl,
+        revealIntroNavControl: revealIntroNavControl,
+        highlighCurrentTrack: highlighCurrentTrack,
+        startAudioControlAnimation: startAudioControlAnimation,
+        pauseAudioControlAnimation: pauseAudioControlAnimation,
+        get audio() { return $audio[0]; }
+    };
+}.call({}));
+
+// ==================== Controller ====================
+IV.Controller = (function() {
+    var audioPlayer;
+
+    function init() {
+        audioPlayer = IV.View.audio;
+        handlePlayerEvents();
+    }
+
+    function handlePlayerEvents() {
+        audioPlayer.addEventListener('ended',function(e){
+            playTrackByTrackNumber(IV.Model.nextTrackNumber);
+        });
+
+        audioPlayer.addEventListener('play',function(e){
+            IV.View.highlighCurrentTrack();
+            IV.View.startAudioControlAnimation();
+        });
+
+        audioPlayer.addEventListener('pause',function(e){
+            IV.View.pauseAudioControlAnimation();
+        });
+    }
+
+    function playTrackByTrackNumber(trackNumber, reload) {
+        if(IV.Model.currentTrackNumber !== trackNumber || reload) {
+            IV.Model.setCurrentTrackByTrackNumber(trackNumber);
+            audioPlayer.src = IV.Model.getPathForTrack(IV.Model.currentTrack);
+            audioPlayer.load();
+        }
+
+        audioPlayer.play();
+    }
+
+    function togglePlay() {
+        if (!audioPlayer.paused) {
+            audioPlayer.pause();
+        } else {
+            playTrackByTrackNumber(IV.Model.currentTrackNumber);
+        }
+    }
+
+    return {
+        init: init,
+        playTrackByTrackNumber: playTrackByTrackNumber,
+        togglePlay: togglePlay
+    }
+
+}.call({}));
+
+// ==================== Config ====================
+var config  = {
+    debug: true,
+    currentVolumnNumber: 1,
+    audioPath: './audio/',
+    volumePathWidth: 3, // # of digits in the path
+    playlistFilename: 'playlist.json'
+};
 
 
-init();
+// ==================== Bootstrap ====================
+IV.init(config);
+
