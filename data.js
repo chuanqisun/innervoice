50ee049d013a919efffa1ec27bc04715da05b86b
Chuanqi <henrysun918@gmail.com>
2016-06-18 12:45:10 -0700

    publish iv3

--- data.js
+++ data.js
@@ -1,20 +1,29 @@
 var Data = 
 {
-    "currentIssueNumber": "2",
-    "playlists": [
+    "currentIssueNumber": "3",
+    "unpublished": [
         {
             "issueNumber": "1",
             "title": "The Ancient Sorrow",
             "dates": "1462651292",
             "intro": "Softly they whispered into darkness, but the Ancient Sorrow echoed with a beam of light.",
-            "embedUrl": "http://8tracks.com/mixes/7936299/player_v3_universal"
+            "embedUrl": "http://8tracks.com/mixes/8062642/player_v3_universal"
         },
         {
             "issueNumber": "2",
             "title": "End of Time",
             "dates": "1464549259",
             "intro": "In the end, time ceased to be, and yet the minds sought hope in the impossible realm of future.",
-            "embedUrl": "http://8tracks.com/mixes/7936299/player_v3_universal"
+            "embedUrl": "http://8tracks.com/mixes/8062642/player_v3_universal"
+        }
+    ],
+    "playlists": [
+        {
+            "issueNumber": "3",
+            "title": "Don't Wake Up",
+            "dates": "1466278310",
+            "intro": "Emotions swallow people whole as that last strand of unreality slips away.",
+            "embedUrl": "http://8tracks.com/mixes/8062642/player_v3_universal"
         }
     ]
 }
